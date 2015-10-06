75300708d4ec4535df7ba037b63a8313e3f1b5ef
Jacob Edelman <edelman.jd@gmail.com>
2015-10-06 19:03:40 +0000

    pyrcb.py

---
+++ sample.py
@@ -0,0 +1,35 @@
+# Written in 2015 by Jacob Edelman based off work by nickolas360 (https://github.com/nickolas360)
+#
+# To the extent possible under law, the author(s) have
+# dedicated all copyright and related and neighboring
+# rights to this software to the public domain worldwide.
+# This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain
+# Dedication along with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
+
+
+from pyrcb import IRCBot
+from datetime import datetime
+import random
+
+chan = "#hsncsclub"
+class ExampleBot(IRCBot):
+    def on_message(self, message, nickname, channel, is_query):
+        self.send(chan, "echoing: " + message)
+
+
+def main():
+    bot = ExampleBot(debug_print=True)
+    bot.connect("irc.pdgn.co", 6697)
+    bot.register("testbot" + str(random.randint(0,1000)))
+    bot.join("#hsncsclub")
+
+    # Blocking; will return when disconnected.
+    bot.listen()
+    print("Disconnected from server.")
+
+if __name__ == "__main__":
+    main()
+
